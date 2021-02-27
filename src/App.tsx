50b9a86194942235c3478110d7f3fea7cd8a4822
Egorych <mikhalevichegor@gmail.com>
2021-02-27 23:17:14 +0300

    React-Redux. Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            This is App component
+            <div>
+                <Rating/>
+                <Accordion/>
+            </div>
+
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
