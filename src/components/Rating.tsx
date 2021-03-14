56e52b9cf5720a7d60ce8475cd0973e65a243837
Egorych <mikhalevichegor@gmail.com>
2021-03-14 18:37:02 +0300

    React-Redux. 05. условный рендеринг. selected value - star

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -7,73 +7,16 @@ type RatingPropsType = {
 
 export function Rating(props: RatingPropsType) {
 
-    if (props.value === 1) {
         return (
             <div>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
+                <Star selected={props.value > 0}/>
+                <Star selected={props.value > 1}/>
+                <Star selected={props.value > 2}/>
+                <Star selected={props.value > 3}/>
+                <Star selected={props.value > 4}/>
             </div>
         )
     }
-    if (props.value === 2) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-            </div>
-        )
-    }
-    if (props.value === 3) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-            </div>
-        )
-    }
-    if (props.value === 4) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-            </div>
-        )
-    }
-    if (props.value === 5) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-            </div>
-        )
-    }
-    return (
-        <div>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-        </div>
-    )
-
-
-}
 export type StarPropsType = {
     selected: boolean
 }
