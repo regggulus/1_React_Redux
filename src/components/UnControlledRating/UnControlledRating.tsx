a0e506bbea0304f6a751eac291b61b6a1fd6ec45
Egorych <mikhalevichegor@gmail.com>
2021-03-15 18:59:08 +0300

    React-Redux. 07 uncontrolled components. UnControlledR .tsx

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 type RatingPropsType = {
-
-    // selected: boolean
+    // selected: () => void
 }
 
 export function UnControlledRating(props: RatingPropsType) {
-let value = 0
+
+    const [value, setValue] = useState(0 | 1 | 2 | 3 | 4)
         return (
-            <div>
-                <Star selected={value > 0}/>
-                <Star selected={value > 1}/>
-                <Star selected={value > 2}/>
-                <Star selected={value > 3}/>
-                <Star selected={value > 4}/>
+            <div >
+                <Star selected={value > 0} /><button onClick={ () => {setValue(1)}}>x</button>
+                <Star selected={value > 1} /><button onClick={ () => {setValue(2)}}>x</button>
+                <Star selected={value > 2} /><button onClick={ () => {setValue(3)}}>x</button>
+                <Star selected={value > 3} /><button onClick={ () => {setValue(4)}}>x</button>
+                <Star selected={value > 4} /><button onClick={ () => {setValue(5)}}>x</button>
+
             </div>
         )
     }
