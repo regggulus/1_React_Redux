9176c3870adb25c20ad4578d434c477836c297c4
Egorych <mikhalevichegor@gmail.com>
2021-03-15 20:21:16 +0300

    React-Redux. 08 callback. we throw the hook useState

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -8,25 +8,25 @@ export function UnControlledRating(props: RatingPropsType) {
     const [value, setValue] = useState(0 | 1 | 2 | 3 | 4)
         return (
             <div >
-                <Star selected={value > 0} />
-                <Star selected={value > 1} />
-                <Star selected={value > 2} />
-                <Star selected={value > 3} />
-                <Star selected={value > 4} />
+                <Star selected={value > 0} setValue={setValue} value={1}/>
+                <Star selected={value > 1} setValue={setValue} value={2}/>
+                <Star selected={value > 2} setValue={setValue} value={3}/>
+                <Star selected={value > 3} setValue={setValue} value={4}/>
+                <Star selected={value > 4} setValue={setValue} value={5}/>
 
             </div>
         )
     }
 export type StarPropsType = {
     selected: boolean
-    value: number
+    value: 0 | 1 | 2 | 3 | 4 | 5
+    setValue: (value: 0 | 1 | 2 | 3 | 4 | 5) => void
 }
 function Star(props: StarPropsType) {
-    return <span onClick={ () => {setValue(1)}}>{props.selected ? <b>star</b> : "star"}</span>
-
-        /*if (props.selected === true) {
-           return <span><b>star </b></span>
-        }else {
-           return <span>star </span>
-        }*/
+    return (
+        <span
+            onClick={ () => {props.setValue(props.value)}}>
+            {props.selected ? <b>star </b> : "star "}
+        </span>
+    )
 }
