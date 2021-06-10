1aa33fbcb1356d32dda46b39291812b29eaa56fc
Egorych <mikhalevichegor@gmail.com>
2021-06-10 22:47:05 +0300

    15 - React кабзда как подробно: Select component useState == state

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {action} from '@storybook/addon-actions';
 import {Select} from "./Select";
 
@@ -6,22 +6,28 @@ export default {
     title: 'Select stories',
     component: Select
 };
-const callback = action('select mode change event fired')
-export const WithValue = () =>
-    <Select onChange={action("Value changed")}
-            value={"1"}
+export const WithValue = () => {
+    const [value, setValue] = useState(null)
+    return (
+    <Select onChange={setValue}
+            value={null}
             items={[
                 {value: "1", title: "Minsk" },
                 {value: "2", title: "Moscow" },
                 {value: "3", title: "Kiev" },
             ]}/>
-export const WithoutValue = () =>
-    <Select onChange={action("Value changed")}
-            value={"1"}
-            items={[
-                {value: "1", title: "Minsk" },
-                {value: "2", title: "Moscow" },
-                {value: "3", title: "Kiev" },
-            ]}/>
-
+    )
+}
+export const WithoutValue = () => {
+    const [value, setValue] = useState('2')
+    return (
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {value: "1", title: "Minsk" },
+                    {value: "2", title: "Moscow" },
+                    {value: "3", title: "Kiev" },
+                ]}/>
+    )
+}
 
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Select.module.css'
 
 type ItemType = {
@@ -7,26 +7,35 @@ type ItemType = {
 }
 export type SelectPropsType = {
     value: any
-    onChange: () => void
+    onChange: (value: any) => void
     items: ItemType[]
 }
 
 export function Select(props: SelectPropsType) {
+    const [active, setActive] = useState(true)
     const selectedItem = props.items.find(i => i.value === props.value)
+    const toggleItems = () => setActive(!active)
+    const onItemClick = (value: any) => {
+        props.onChange(value)
+        toggleItems()
+    }
     return (
         <>
-            <select name="" id="">
-                <option value=""></option>
-                <option value="">Minsk</option>
-                <option value="">Moscow</option>
-                <option value="">Kiev</option>
-            </select>
-            <div className={styles.select + " " + styles.active}>
-
-                <h3>{selectedItem && selectedItem.title}</h3>
-                <div className={styles.items}>
-                {props.items.map(i => <div key={i.value}>{i.title}</div>)}
-                </div>
+            <div
+                className={styles.select}>
+                <span className={styles.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+                {
+                    active &&
+                    <div className={styles.items}>
+                        {props.items.map(i => <div
+                            key={i.value}
+                            onClick={ () => { onItemClick(i.value);
+                            }}
+                            >{i.title}
+                        </div>
+                        )}
+                    </div>
+                }
             </div>
         </>
     )
