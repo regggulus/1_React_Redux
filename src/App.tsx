70bbd29d395cf25321ae6f7debf092f73d7e00ea
Egorych <mikhalevichegor@gmail.com>
2021-07-20 22:55:18 +0300

    New2

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Accordion} from './components/Accordion/Accordion';
 import {Rating, RatingValueType} from "./components/Rating/Rating";
-import {OnOff} from "./components/OnOff/OnOff";
 import {UnControlledAccordion} from "./components/UnControlledAccordion/UnControlledAccordion";
-import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
-import {UnControlledOnOff} from "./components/UnControlledOnOff/UnControlledOnOff";
 
 type AppPropsType = {
 
 }
 
 function App(props: AppPropsType) {
-    const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
-    const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
-    const [switchOn, setSwitchOn] = useState<boolean>(false)
-    const [unSwitchOn, setUnSwitchOn] = useState<boolean>(false)
     return (
         <div className="App">
             <div>
-                <OnOff on={switchOn} onChange={setSwitchOn}/>
-
-                <PageTitle title={"This is App component"}/>
-                <PageTitle title={"My Friends"}/>
-
-                <hr/><h3>Un controlled</h3><hr/>
-                <UnControlledOnOff onChange={setUnSwitchOn}/>
-
-                {/*<UnControlledRating />*/}
 
                 <UnControlledAccordion title={'Menu'}/>
                 <UnControlledAccordion title={'Users'}/>
 
                 <hr/><h3>Controlled</h3><hr/>
-
-
-                <Rating
-                    value={ratingValue}
-                    onClick={setRatingValue} />
 
             </div>
 
