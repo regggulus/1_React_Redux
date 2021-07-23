34a9412f3a7b2444f023f87914ffb7d0b7a9b3c4
Egorych <mikhalevichegor@gmail.com>
2021-07-23 13:28:14 +0300

    function Star

--- src/Accordion.tsx
+++ src/Accordion.tsx
@@ -3,7 +3,24 @@ import React from "react";
 export function Accordion() {
     return (
         <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return (
+        <div>
             <h3>Menu</h3>
+        </div>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <div>
             <ul>
                 <li>1</li>
                 <li>2</li>
