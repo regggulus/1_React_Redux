973dad03ebaafc631dbf38cd169d054189a07f3f
Egorych <mikhalevichegor@gmail.com>
2021-07-23 18:15:32 +0300

    directory components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import {Rating} from "./Rating";
-import {Accordion} from "./Accordion";
+import {Rating} from "./components/Rating";
+import {Accordion} from "./components/Accordion";
 
 
 export function App() {
