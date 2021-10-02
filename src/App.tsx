4dd5bb038c9f25a46db7715526f96e3cc0d4bc82
Egorych <mikhalevichegor@gmail.com>
2021-10-02 17:21:51 +0300

    new commit React-Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Rating} from "./components/Rating";
-import {Accordion} from "./components/Accordion";
-
 
 export function App() {
     return (
         <div className={"App"}>
-            <PageTitle title={"This is App component"}/>
-            <PageTitle title={"Users"}/>
-            <Rating value={3}/>
-            <Accordion title={"Menu"}/>
-            <Accordion title={"List"}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
 
         </div>
     )
 }
-
-function PageTitle(props: any) {
-    return(
-        <div>
-            <h3>{props.title}</h3>
-        </div>
-    )
-}
-
-
 
