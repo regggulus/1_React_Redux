087f68bcab0446e480fdb6eb9e2d2420ee464f2e
Egorych <mikhalevichegor@gmail.com>
2021-10-02 18:37:53 +0300

    component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,34 @@ import './App.css';
 export function App() {
     return (
         <div className={"App"}>
+            <h2>This is App component</h2>
+            <Rating/>
+            <Accordion/>
+        </div>
+    )
+}
 
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+function Accordion () {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
         </div>
     )
 }
