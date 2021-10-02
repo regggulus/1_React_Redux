9202c1688d193ae407df783bc5c1ffcb63c80597
Egorych <mikhalevichegor@gmail.com>
2021-10-02 18:57:01 +0300

    component Rating.tsx, Accordion.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Rating} from "./components/rating/Rating";
+import {Accordion} from "./components/accordion/Accordion";
 
 export function App() {
     return (
         <div className={"App"}>
             <h2>This is App component</h2>
             <Rating/>
+            <Rating/>
+            <Rating/>
+            <Rating/>
+            <Rating/>
             <Accordion/>
-        </div>
-    )
-}
-
-function Rating() {
-    return (
-        <div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-        </div>
-    )
-}
-
-function Accordion () {
-    return (
-        <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
         </div>
     )
 }
---
+++ src/components/rating/Rating.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Rating() {
+    return (
+        <div>
+            <div>star</div>
+        </div>
+    )
+}
