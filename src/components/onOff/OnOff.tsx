f4ae514dd181c3158ee0ef36f78491852e0a2542
Egorych <mikhalevichegor@gmail.com>
2021-10-28 23:57:58 +0300

    06 (useState)/ component OnOff.tsx  css and props

--- src/components/onOff/OnOff.tsx
+++ src/components/onOff/OnOff.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {on} from "cluster";
 
 type OnOffPropsType = {
     on: boolean
@@ -6,12 +7,39 @@ type OnOffPropsType = {
 
 export function OnOff(props: OnOffPropsType) {
 
+const onStyle = {
+    width: "30px",
+    height: "15px",
+    border: "1px solid black",
+    margin: "10px 0px 5px 15px",
+    display: "inline-block",
+    backgroundColor: props.on ? "greenyellow" : "white"
+}
 
+const offStyle = {
+    width: "30px",
+    height: "15px",
+    border: "1px solid black",
+    marginLeft: "5px",
+    display: "inline-block",
+    backgroundColor: props.on ? "white" : "coral"
+}
+
+const indicatorStyle = {
+    width: "10px",
+    height: "10px",
+    borderRadius: "5px",
+    border: "1px solid black",
+    display: "inline-block",
+    marginLeft: "5px",
+    backgroundColor: props.on ? "greenyellow" : "coral"
+
+}
     return(
         <div>
-            <div>On</div>
-            <div>Off</div>
-            <div></div>
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
         </div>
     )
 }
